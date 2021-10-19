84499320b02e28253f1b0431399ad9ce95132187
Commandtechno <68407783+commandtechno@users.noreply.github.com>
2021-10-19 11:06:46 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+stolen from https://github.com/drawrowfly/tiktok-scraper/blob/master/src/constant/index.ts#L36
+
+idk im using this for something its kinda funny
---
+++ index.ts
@@ -0,0 +1,28 @@
+export const os = [
+  "Macintosh; Intel Mac OS X 10_15_7",
+  "Macintosh; Intel Mac OS X 10_15_5",
+  "Macintosh; Intel Mac OS X 10_11_6",
+  "Macintosh; Intel Mac OS X 10_6_6",
+  "Macintosh; Intel Mac OS X 10_9_5",
+  "Macintosh; Intel Mac OS X 10_10_5",
+  "Macintosh; Intel Mac OS X 10_7_5",
+  "Macintosh; Intel Mac OS X 10_11_3",
+  "Macintosh; Intel Mac OS X 10_10_3",
+  "Macintosh; Intel Mac OS X 10_6_8",
+  "Macintosh; Intel Mac OS X 10_10_2",
+  "Macintosh; Intel Mac OS X 10_10_3",
+  "Macintosh; Intel Mac OS X 10_11_5",
+  "Windows NT 10.0; Win64; x64",
+  "Windows NT 10.0; WOW64",
+  "Windows NT 10.0",
+];
+
+export function generate() {
+  return `Mozilla/5.0 (${
+    os[Math.floor(Math.random() * os.length)]
+  }) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${
+    Math.floor(Math.random() * 3) + 87
+  }.0.${Math.floor(Math.random() * 190) + 4100}.${
+    Math.floor(Math.random() * 50) + 140
+  } Safari/537.36`;
+}
